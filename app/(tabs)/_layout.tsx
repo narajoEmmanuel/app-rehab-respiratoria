118c77bfe304dd3d59a53b03c6e2c90de174ee97
Emmanuel Naranjo <naranjo760emm@gmail.com>
2026-05-07 13:50:07 -0600

    revert(navigation): remove experimental tab swipe gestures

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -100,6 +100,7 @@ export default function TabLayout() {
         options={{
           title: 'Sesión',
           href: null,
+          tabBarStyle: { display: 'none' },
         }}
       />
       <Tabs.Screen
